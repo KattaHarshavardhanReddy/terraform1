44080059d7d7fcbbb81d06594a68d2278df856bf
KattaHarshavardhanReddy <katta.harshareddy@gmail.com>
2025-07-29 14:58:27 +0530

    terraform

--- terraform-aws-vpc/variable.tf
+++ terraform-aws-vpc/variable.tf
@@ -18,3 +18,68 @@ variable "common_tags" {
     type = map 
     default = {}
 }
+
+variable "igw_tags" {
+    
+    default = {}
+}
+
+variable "public_subnet_cidr" {
+    type = list 
+    validation {
+        condition = length(var.public_subnet_cidr)  == 2
+        error_message = "please provide min 2 subnet id's"
+    }
+}
+
+variable "public_subnet_tags" {
+    default = {}
+}
+
+variable "private_subnet_cidr" {
+    type = list 
+    validation {
+        condition = length(var.private_subnet_cidr)  == 2
+        error_message = "please provide min 2 subnet id's"
+    }
+}
+
+variable "private_subnet_tags" {
+    default = {}
+}
+
+variable "database_subnet_cidr" {
+    type = list 
+    validation {
+        condition = length(var.database_subnet_cidr)  == 2
+        error_message = "please provide min 2 subnet id's"
+    }
+}
+
+variable "database_subnet_tags" {
+    default = {}
+}
+
+variable "nat_gw_tags" {
+    default = {}
+}
+
+variable "public_route_tags" {
+    default = {}
+}
+
+variable "private_route_tags" {
+    default = {}
+}
+
+variable "database_route_tags" {
+    default = {}
+}
+
+variable "is_peering_required" {
+    default = false
+}
+
+variable "peering_tags" {
+    default = {}
+}
